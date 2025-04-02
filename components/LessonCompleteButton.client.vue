157c536f102053871022f9402156bd7e3edbed1c
Artem Bohuslavsky <newlifeboguslavskiy@gmail.com>
2025-04-02 12:39:12 +0300

    init project

---
+++ components/LessonCompleteButton.client.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+defineEmits<{
+  (e: "update:modelValue", payload: boolean): void;
+}>();
+</script>
+
+<template>
+  <label
+    class="rounded text-white font-bold py-2 px-4 cursor-pointer"
+    :class="{
+      'bg-green-500': modelValue,
+      'bg-gray-500': !modelValue,
+    }">
+    <input
+      type="checkbox"
+      :value="modelValue"
+      class="hidden"
+      @input="() => $emit('update:modelValue', !modelValue)" />
+    {{ modelValue ? "Completed!" : "Mark as complete" }}
+  </label>
+</template>
+
+<style scoped>
+::selection {
+  display: none;
+}
+</style>
